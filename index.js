295af13e279b6928a0a647eb5f373fab89ef766a
Jeral Pineda <114681856+JeralP@users.noreply.github.com>
2022-10-05 08:40:57 -0600

    Agregando el contenido del proyecto

---
+++ index.js
@@ -0,0 +1,44 @@
+import fs from "fs-extra";
+import axios from "axios";
+import { getImageSize } from "./getImageSize";
+import { log, time } from "./log";
+
+const INITIAL_ID_XKCD_COMIC = 2580;
+const MAX_ID_XKCD_COMIC = 2588;
+
+let options = {
+  proxy: {
+    host: "192.168.0.1",
+    port: 8080,
+  },
+};
+
+const endTime = time();
+
+const { writeJSON } = fs;
+
+for (let id = INITIAL_ID_XKCD_COMIC; id < MAX_ID_XKCD_COMIC; id++) {
+  const url = `https://xkcd.com/${id}/info.0.json`;
+
+  log(`Fetching ${url}...`);
+
+  // const data = getData(url);
+  const { data } = await axios.get(url);
+  const { num, news, transcript, ...restOfComic } = data;
+  log(`Fetched #${num}. Getting image dimensions...`);
+  const { height, width } = await getImageSize({ url: img });
+  log(`Got image dimensions: ${width}x${height}`);
+
+  const comicToStore = {
+    id,
+    img,
+    height,
+    width,
+    ...restOfComic,
+  };
+  const jsonFile = `./comics/${id}`;
+  await writeJSON(jsonFile, comicToStore);
+  log(`Wrote ${jsonFile}! ✅\n`);
+}
+
+endTime();
